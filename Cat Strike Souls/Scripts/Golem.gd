d58cc3d98bdce3f18a9ec497d0f8de1b30ab86a5
Guilherme Prigol Kramer <guilhermeprigolkramer@gmail.com>
2023-08-16 23:32:48 -0300

    a

--- Cat Strike Souls/Scripts/Golem.gd
+++ Cat Strike Souls/Scripts/Golem.gd
@@ -5,9 +5,13 @@ var spd = 50
 var limit_spd = 100
 var desac = 10
 
+var rng = RandomNumberGenerator.new()
+var dir
+var savedDir
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	dir = rng.randi_range(1, 8)
 
 func _process(delta):
 	if motion.x > limit_spd:
@@ -20,8 +24,28 @@ func _process(delta):
 	elif motion.y < -limit_spd:
 		motion.y = -limit_spd
 		
-	motion.x += spd
-	
+		
+	match dir:
+		1:
+			motion.y -= spd
+		2:
+			motion.y -= spd
+			motion.x += spd
+		3:
+			motion.x += spd
+		4:
+			motion.x += spd
+			motion.y += spd
+		5:
+			motion.y += spd
+		6:
+			motion.x -= spd
+			motion.y += spd
+		7:
+			motion.x -= spd
+		8:
+			motion.x -= spd
+			motion.y -= spd
 	
 	move_and_slide(motion)
 
@@ -29,3 +53,13 @@ func _process(delta):
 func _on_GolemArea2D_body_entered(body):
 	if body.name == 'Player':
 		print('Player encostou no Boss')
+	else:
+		print('Boss encostou em ', body.name, ' ')
+		savedDir = dir
+		while dir == savedDir:
+			dir = rng.randi_range(1, 8)
+
+
+func _on_Influence_body_entered(body):
+	if body.name == 'Player':
+		$AnimatedSprite.play("attack")
