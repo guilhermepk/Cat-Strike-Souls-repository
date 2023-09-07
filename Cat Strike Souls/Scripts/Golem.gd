3c96beaea7e2482d079390fe052b054f0f7c5c11
Guilherme Prigol Kramer <guilhermeprigolkramer@gmail.com>
2023-09-06 22:19:20 -0300

    novo ataque, meio roubado kkkkk

--- Cat Strike Souls/Scripts/Golem.gd
+++ Cat Strike Souls/Scripts/Golem.gd
@@ -2,7 +2,7 @@ extends KinematicBody2D
 
 var motion = Vector2.ZERO
 var spd = 50
-var limit_spd = 150
+var limit_spd = 100
 var desac = 10
 
 var rng = RandomNumberGenerator.new()
@@ -10,8 +10,9 @@ var rng = RandomNumberGenerator.new()
 var follow
 var player
 
-var playerInArm = false
+var playerInInfluence = false
 var playerNearby = false
+var playerInArm = false
 var playerTooNear = false
 
 # Called when the node enters the scene tree for the first time.
@@ -37,11 +38,18 @@ func _process(delta):
 		motion.y = 0
 		
 	if playerInArm and $AnimatedSprite.animation == 'attack' and $AnimatedSprite.frame == 7:
-		print('Jogador atacado')
+		print('Jogador atacado por garra')
 		player.alive = false
+	
+	var animationTime = $AnimationPlayer.current_animation_position
+	if animationTime >= 1.25 and animationTime <= 1.75:
+		if playerNearby:
+			print('Jogador atacado por explosão em ', animationTime, ' segundos de explosão')
+			player.alive = false
 	
 	if playerTooNear and player.alive:
 		$AnimatedSprite.play("shine")
+		$AnimationPlayer.play("shineAttack")
 	elif playerNearby and player.alive:
 		$AnimatedSprite.play("attack")
 	else:
@@ -77,7 +85,7 @@ func followPlayer(player):
 	
 	var xInside
 	var yInside
-	if playerX < x+30*scale.x and playerX > x-35*scale.x:
+	if playerX < x+30*scale.x and playerX > x-30*scale.x:
 		xInside = true
 	if playerY < y+5*scale.x and playerY > y-5*scale.x:
 		yInside = true
@@ -96,12 +104,15 @@ func _on_Influence_body_entered(body):
 		player = body
 		print(player)
 		follow = true
+		playerInInfluence = true
 
 
 func _on_Influence_body_exited(body):
 	if body.name == 'Player':
 		print('Player saiu da área de influência')
 		follow = false
+		playerInInfluence = false
+		
 
 
 func _on_InfluenceNearby_body_entered(body):
@@ -121,7 +132,6 @@ func _on_Arm_body_entered(body):
 	if body.name == 'Player':
 		playerInArm = true
 		player = body
-	
 
 
 func _on_Arm_body_exited(body):
