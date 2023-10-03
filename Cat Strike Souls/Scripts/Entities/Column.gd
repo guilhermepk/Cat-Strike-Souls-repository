a28a20c0ac32891e345ce57e69c1485607d83049
Guilherme Prigol Kramer <guilhermeprigolkramer@gmail.com>
2023-10-03 01:24:36 -0300

    eu consegui * emoji de joinha *

---
+++ Cat Strike Souls/Scripts/Entities/Column.gd
@@ -0,0 +1,61 @@
+extends Node2D
+
+var playerAround
+var player
+var XKey
+
+var symbols
+var reversedSymbols = []
+var maxEnergy = 0.6
+var currentSymbolIndex = 0
+var fullyCharged = false
+
+func charge():
+	var currentSymbol = reversedSymbols[currentSymbolIndex]
+	var currentLight = currentSymbol.get_node('Light2D')
+	var currentEnergy = currentLight.energy
+	if currentEnergy >= maxEnergy == false:
+		currentLight.energy += 0.01
+		var energyPercent = (currentEnergy * 100) / maxEnergy
+		var currentAlpha = (1 * energyPercent) / 100
+		currentSymbol.modulate.a = currentAlpha
+		
+	else:
+		currentSymbolIndex += 1
+		
+func _ready():
+	player = get_tree().current_scene.get_node('Elements/YSort/Player')
+	XKey = player.get_node('XKey')
+	
+	symbols = $TileMap/LightEffects.get_children()
+	for symbol in symbols:
+		reversedSymbols.insert(0, symbol)
+		symbol.visible = true
+		symbol.modulate.a = 0
+		symbol.get_node('Light2D').energy = 0
+		symbol.scale.x = 1
+		symbol.scale.y = 1
+	
+func _process(delta):
+	
+	if fullyCharged:
+		$AnimationPlayer.play("charged")
+	elif playerAround:
+		if currentSymbolIndex < symbols.size():
+			if Input.is_key_pressed(88): # 88 = x
+				charge()
+		else:
+			XKey.visible = false
+			fullyCharged = true
+		
+				
+func _on_Area2D_body_entered(body):
+	if body.name == 'Player':
+		playerAround = true
+		XKey.visible = true
+		
+
+func _on_Area2D_body_exited(body):
+	if body.name == 'Player':
+		playerAround = false
+		XKey.visible = false
