e3c9a57f7aa316b8be3371d1f0fd3dc649fdd8bc
Guilherme Prigol Kramer <guilhermeprigolkramer@gmail.com>
2023-10-09 21:16:09 -0300

    correção de bug de porta

--- Cat Strike Souls/Scripts/Entities/DoorTunnelToArena.gd
+++ Cat Strike Souls/Scripts/Entities/DoorTunnelToArena.gd
@@ -1,13 +1,5 @@
 extends Area2D
 
-var current_scene
-
-func _ready():
-	current_scene = get_tree().current_scene.filename
-	
-func _process(delta):
-	pass
-
 func _on_Door_body_entered(body):
 	if body.name == 'Player':
 		print('player entrou na porta')
